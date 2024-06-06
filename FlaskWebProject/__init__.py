e869928ec7f90d1278c5247bf87cc139ba505478
Quan Nguyen <QuanNCT@outlook.com>
2024-06-07 00:00:33 +0700

    comit

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,7 +7,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-import FlaskWebProject.views
 
 app = Flask(__name__)
 app.config.from_object(Config)
